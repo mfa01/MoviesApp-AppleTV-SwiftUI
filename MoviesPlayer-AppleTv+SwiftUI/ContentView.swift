ccf6fc0d1cafc487d18abcd5206e2609ec65e7de
Mohammad Alabed <mfa01@yahoo.com>
2022-07-02 15:24:34 +0300

    Initial Commit

---
+++ MoviesPlayer-AppleTv+SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MoviesPlayer-AppleTv+SwiftUI
+//
+//  Created by Mabed on 02/07/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MoviesPlayer-AppleTv+SwiftUI/MoviesPlayer_AppleTv_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoviesPlayer_AppleTv_SwiftUIApp.swift
+//  MoviesPlayer-AppleTv+SwiftUI
+//
+//  Created by Mabed on 02/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct MoviesPlayer_AppleTv_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
